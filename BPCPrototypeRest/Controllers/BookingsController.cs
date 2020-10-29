acf7d4c7f0b872188f2faec15a23b75ff0113e34
Philip Lind Jensen <jogjensen@hotmail.com>
2020-10-29 10:42:12 +0100

    Add project files.

---
+++ BPCPrototypeRest/Controllers/BookingsController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using BPCPrototypeRest.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Features;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BPCPrototypeRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingsController : ControllerBase
+    {
+
+
+        #region Temporary static list
+        private static int idCount = 1;
+
+        private static readonly List<Bookings> bookingsList = new List<Bookings>()
+        {
+            new Bookings(idCount++, "Slagelse", "10:00", "12/12/2020", "Roskilde", "12:00", "13/12/2020", "2", "Gravko",
+                "Dyret står til højre for siloen"),
+            new Bookings(idCount++, "Odense", "12:00", "14/12/2020", "Kastrup", "17:00", "14/12/2020","3" , "Skur",
+                "BOX NR 011")
+        };
+
+        #endregion
+
+        // GET: api/<BookingsController>
+        [HttpGet]
+        public IEnumerable<Bookings> Get()
+        {
+            return bookingsList;
+        }
+
+        // GET api/<BookingsController>/5
+        [HttpGet("{ordNr}")]
+        public Bookings Get(int ordNr)
+        {
+            return bookingsList.Find(o => o.OrdNr == ordNr);
+        }
+
+        // POST api/<BookingsController>
+        [HttpPost]
+        public void Post([FromBody] Bookings value)
+        {
+            value.OrdNr = idCount++;
+            bookingsList.Add(value);
+        }
+
+        // PUT api/<BookingsController>/5
+        [HttpPut("{id}")]
+        public int Put(int id, [FromBody] Bookings value)
+        {
+            Bookings bookings = Get(id);
+            if (bookings != null)
+            {
+                bookings.OrdNr = value.OrdNr;
+                bookings.StartAdr = value.StartAdr;
+                bookings.StartTime = value.StartTime;
+                bookings.StartDate = value.StartDate;
+
+                bookings.EndAdr = value.EndAdr;
+                bookings.EndTime = value.EndTime;
+                bookings.EndDate = value.EndDate;
+
+                bookings.NumberOcn = value.NumberOcn;
+                bookings.TypeOfGoods = value.TypeOfGoods;
+                bookings.Comments = value.Comments;
+
+                return 1;
+            }
+
+            return 0;
+        }
+
+        // DELETE api/<BookingsController>/5
+        [HttpDelete("{id}")]
+        public int Delete(int id)
+        {
+            Bookings bookings = Get(id);
+            if (bookings != null)
+            {
+                BookingsController.bookingsList.Remove(bookings);
+                return 1;
+            }
+
+            return 0;
+        }
+
+
+
+}
+}
---
+++ BPCPrototypeRest/Model/Bookings.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BPCPrototypeRest.Model
+{
+    public class Bookings
+    {
+        #region Instance fields
+        private int _ordNr;
+        //Start information
+        private string _startAdr;
+        private string _startTime;
+        private string _startDate;
+
+        //End information
+        private string _endAdr;
+        private string _endTime;
+        private string _endDate;
+
+        //Generel information
+        private string _numberOCN;
+        private string _typeOfGoods;
+        private string _comments;
+        #endregion
+
+        #region Contructors
+
+        public Bookings(int ordNr, string startAdr, string startTime, string startDate, string endAdr, string endTime, string endDate, string numberOcn, string typeOfGoods, string comments)
+        {
+            _ordNr = ordNr;
+            _startAdr = startAdr;
+            _startTime = startTime;
+            _startDate = startDate;
+            _endAdr = endAdr;
+            _endTime = endTime;
+            _endDate = endDate;
+            _numberOCN = numberOcn;
+            _typeOfGoods = typeOfGoods;
+            _comments = comments;
+        }
+
+        public Bookings()
+        {
+
+        }
+        #endregion
+
+        #region Properties
+
+        public int OrdNr
+        {
+            get => _ordNr;
+            set => _ordNr = value;
+        }
+
+        public string StartAdr
+        {
+            get => _startAdr;
+            set => _startAdr = value;
+        }
+
+        public string StartTime
+        {
+            get => _startTime;
+            set => _startTime = value;
+        }
+
+        public string StartDate
+        {
+            get => _startDate;
+            set => _startDate = value;
+        }
+
+        public string EndAdr
+        {
+            get => _endAdr;
+            set => _endAdr = value;
+        }
+
+        public string EndTime
+        {
+            get => _endTime;
+            set => _endTime = value;
+        }
+
+        public string EndDate
+        {
+            get => _endDate;
+            set => _endDate = value;
+        }
+
+        public string NumberOcn
+        {
+            get => _numberOCN;
+            set => _numberOCN = value;
+        }
+
+        public string TypeOfGoods
+        {
+            get => _typeOfGoods;
+            set => _typeOfGoods = value;
+        }
+
+        public string Comments
+        {
+            get => _comments;
+            set => _comments = value;
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        public override string ToString()
+        {
+            return $"{nameof(OrdNr)}: {OrdNr}, {nameof(StartAdr)}: {StartAdr}, {nameof(StartTime)}: {StartTime}, {nameof(StartDate)}: {StartDate}, {nameof(EndAdr)}: {EndAdr}, {nameof(EndTime)}: {EndTime}, {nameof(EndDate)}: {EndDate}, {nameof(NumberOcn)}: {NumberOcn}, {nameof(TypeOfGoods)}: {TypeOfGoods}, {nameof(Comments)}: {Comments}";
+        }
+
+        #endregion
+
+
+
+    }
+}
